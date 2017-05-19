3ddb69fc893283fa5227de631eb379574ed10cca
zimrick <r.zimerman33@gmail.com>
2017-05-19 15:59:54 +0200

    Added new Markers component

--- src/Markers.js
+++ src/Markers.js
@@ -1,42 +1,36 @@
 
-import React, { PropTypes } from "react"
-import defaultStyles from "./defaultStyles"
-import Marker from "./Marker"
+import React, { Component } from "react"
 
-class Markers extends React.Component {
+class Markers extends Component {
   render() {
+
+    const {
+      children,
+      projection,
+      style,
+    } = this.props
+
     return (
-      <g className="rsm-markers">
+      <g className="rsm-markers" style={ style }>
         {
-          this.props.markers.map((marker, i) => (
-            <Marker
-              key={ `${Math.abs(marker.coordinates[0])}-${Math.abs(marker.coordinates[1])}-${i}` }
-              projection={ this.props.projection }
-              marker={ marker }
-              styles={ this.props.styles.marker }
-              events={ this.props.events }
-              zoom={ this.props.zoom }
-            />
-          ))
+          !children.length ?
+            React.cloneElement(children, {
+              projection,
+            }) :
+            children.map((child, i) => (
+              React.cloneElement(child, {
+                key: child.key || `marker-${i}`,
+                projection,
+              })
+            ))
         }
       </g>
     )
   }
 }
 
-Markers.propTypes = {
-  markers: PropTypes.array,
-  projection: PropTypes.func.isRequired,
-  zoom: PropTypes.number,
-  events: PropTypes.object,
-  styles: PropTypes.object,
-}
-
 Markers.defaultProps = {
-  markers: [],
-  zoom: 1,
-  events: {},
-  styles: defaultStyles,
+  componentIdentifier: "Markers",
 }
 
 export default Markers
