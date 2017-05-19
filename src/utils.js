1ac8f0542010cc93ca550a9e942bcb896fc7187b
zimrick <r.zimerman33@gmail.com>
2017-05-19 15:55:08 +0200

    Added new utils

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
 
+import React from "react"
+
 export function replaceStrokeWidth(styles) {
   let newStyles = {}
   Object.keys(styles).map((key, i) => {
@@ -26,8 +28,57 @@ export function calculateResizeFactor(actualDimension, baseDimension) {
   return 1 / 100 * (100 / actualDimension * baseDimension)
 }
 
-export function calculateMousePosition(direction, projection, props, zoom, resizeFactor) {
-  const { center, width, height } = props
+export function calculateMousePosition(direction, projection, props, zoom, resizeFactor, center = props.center, width = props.width, height = props.height) {
   const reference = { x: 0, y: 1 }
-  return (projection()([-center[0],-center[1]])[reference[direction]] - (reference[direction] === 0 ? width : height) / 2) * zoom * (1/resizeFactor)
+  const reverseRotation = projection().rotate().map(item => -item)
+  return (projection().rotate(reverseRotation)([-center[0],-center[1]])[reference[direction]] - (reference[direction] === 0 ? width : height) / 2) * zoom * (1/resizeFactor)
+}
+
+export function isChildOfType(child, expectedType) {
+  return child.props.componentIdentifier === expectedType
+}
+
+export function createNewChildren(children, props) {
+  if (!children.length) {
+    return isChildOfType(children, "Geographies") ? React.cloneElement(children, {
+      geographyPaths: props.geographyPaths,
+      projection: props.projection,
+    }) : (isChildOfType(children, "Markers") || isChildOfType(children, "Annotation") ? React.cloneElement(children, {
+      projection: props.projection,
+      zoom: props.zoom,
+    }) : children)
+  }
+  else {
+    return children.map((child, i) => {
+      return isChildOfType(child, "Geographies") ?
+        React.cloneElement(child, {
+          key: `zoomable-child-${i}`,
+          geographyPaths: props.geographyPaths,
+          projection: props.projection,
+        }) : (isChildOfType(child, "Markers") || isChildOfType(child, "Annotation") ?
+        React.cloneElement(child, {
+          key: `zoomable-child-${i}`,
+          projection: props.projection,
+          zoom: props.zoom,
+        }): child)
+    })
+  }
+}
+
+export function roundPath(path, precision) {
+  const query = /[\d\.-][\d\.e-]*/g
+  return path.replace(query, n => Math.round(n * (1/precision)) / (1/precision))
+}
+
+export function createConnectorPath(connectorType, endPoint) {
+  return `M0,0 L${endPoint[0]},${endPoint[1]}`
+}
+
+export function createTextAnchor(dx) {
+  if (dx > 0)
+    return "start"
+  else if (dx < 0 )
+    return "end"
+  else
+    return "middle"
 }
