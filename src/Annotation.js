b6268444efda981bb64720057c5fe42864e77bf1
zimrick <r.zimerman33@gmail.com>
2017-05-19 15:41:51 +0200

    Exposed separate components

---
+++ src/Annotation.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from "react"
+
+class Annotation extends Component {
+  render() {
+    return (
+      <g></g>
+    )
+  }
+}
+
+export default Annotation
---
+++ src/ComposableMap.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from "react"
+
+class ComposableMap extends Component {
+  render() {
+    return (
+      <g></g>
+    )
+  }
+}
+
+export default ComposableMap
