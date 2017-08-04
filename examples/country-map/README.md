f3e9ffa4f9eda1737731b238fa52dad540f75cc0
zimrick <r.zimerman33@gmail.com>
2017-08-04 19:44:53 +0200

    Added country-map example

---
+++ examples/country-map/README.md
@@ -0,0 +1,20 @@
+
+# Country Map with Subnational Administrative Units
+
+Example of using `react-simple-maps` to map out a country with subnational administrative units.
+
+## How to use
+
+Download this example map [or clone the repo](https://github.com/zcreativelabs/react-simple-maps):
+
+```bash
+curl https://codeload.github.com/zcreativelabs/react-simple-maps/tar.gz/master | tar -xz --strip=2 react-simple-maps-master/examples/country-map
+cd country-map
+```
+
+Once you have downloaded the example install the dependencies and run the app.
+
+```bash
+npm install
+npm run dev
+```
---
+++ examples/country-map/pages/index.js
@@ -0,0 +1,69 @@
+
+import React, { Component } from "react"
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+} from "react-simple-maps"
+
+const wrapperStyles = {
+  width: "100%",
+  maxWidth: 980,
+  margin: "0 auto",
+}
+
+class CountryMap extends Component {
+  render() {
+    return (
+      <div style={wrapperStyles}>
+        <ComposableMap
+          projection="mercator"
+          projectionConfig={{ scale: 10000 }}
+          width={980}
+          height={551}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          >
+          <ZoomableGroup center={[ 8.2, 46.8 ]} disablePanning>
+            <Geographies geographyUrl="/static/ch-with-cantons.json">
+              {(geographies, projection) =>
+                geographies.map((geography, i) =>
+                  geography.id !== "ATA" && (
+                    <Geography
+                      key={i}
+                      geography={geography}
+                      projection={projection}
+                      style={{
+                        default: {
+                          fill: "#ECEFF1",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        hover: {
+                          fill: "#607D8B",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        pressed: {
+                          fill: "#FF5722",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                      }}
+                    />
+              ))}
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    )
+  }
+}
+
+export default CountryMap
