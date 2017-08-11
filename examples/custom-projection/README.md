fed8ea675c82e22cdab9d57b323df8b95aaf0f2c
this-is-cris <cristinapoiata@hotmail.com>
2017-08-11 19:42:57 +0200

    Added custom-projection example

---
+++ examples/custom-projection/README.md
@@ -0,0 +1,20 @@
+
+# Custom Projection
+
+Example of using `react-simple-maps` to create a map of Europe using a custom projection from `d3-geo` ([Azimuthal Equal Area](https://github.com/d3/d3-geo-projection#geoAzimuthalEqualArea)).
+
+## How to use
+
+Download this example map [or clone the repo](https://github.com/zcreativelabs/react-simple-maps):
+
+```bash
+curl https://codeload.github.com/zcreativelabs/react-simple-maps/tar.gz/master | tar -xz --strip=2 react-simple-maps-master/examples/custom-projection
+cd custom-projection
+```
+
+Once you have downloaded the example install the dependencies and run the app.
+
+```bash
+npm install
+npm run dev
+```
---
+++ examples/custom-projection/pages/index.js
@@ -0,0 +1,77 @@
+
+import React, { Component } from "react"
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+  Graticule,
+} from "react-simple-maps"
+import { geoAzimuthalEqualArea } from "d3-geo"
+
+const wrapperStyles = {
+  width: "100%",
+  maxWidth: 980,
+  margin: "0 auto",
+}
+
+class CustomProjectionMap extends Component {
+  projection(width, height, config) {
+    return geoAzimuthalEqualArea()
+      .rotate([-10,-52,0])
+      .scale(config.scale)
+  }
+  render() {
+    return (
+      <div style={wrapperStyles}>
+        <ComposableMap
+          projection={this.projection}
+          projectionConfig={{
+            scale: 600,
+          }}
+          width={980}
+          height={551}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          >
+          <ZoomableGroup center={[10,52]}>
+            <Geographies geographyUrl="/static/world-50m.json">
+              {(geographies, projection) => geographies.map((geography, i) => (
+                <Geography
+                  key={i}
+                  geography={geography}
+                  projection={projection}
+                  style={{
+                    default: {
+                      fill: "#ECEFF1",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#607D8B",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#FF5722",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                  }}
+                />
+              ))}
+            </Geographies>
+            <Graticule />
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    )
+  }
+}
+
+export default CustomProjectionMap
