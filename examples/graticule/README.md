234a8190774979c00b6d11fd4099e927fbf85511
zimrick <r.zimerman33@gmail.com>
2017-08-11 18:58:17 +0200

    Added graticule example

---
+++ examples/graticule/README.md
@@ -0,0 +1,20 @@
+
+# Graticule
+
+Example of using `react-simple-maps` to create a basic world map with a graticule.
+
+## How to use
+
+Download this example map [or clone the repo](https://github.com/zcreativelabs/react-simple-maps):
+
+```bash
+curl https://codeload.github.com/zcreativelabs/react-simple-maps/tar.gz/master | tar -xz --strip=2 react-simple-maps-master/examples/graticule
+cd graticule
+```
+
+Once you have downloaded the example install the dependencies and run the app.
+
+```bash
+npm install
+npm run dev
+```
---
+++ examples/graticule/pages/index.js
@@ -0,0 +1,71 @@
+
+import React, { Component } from "react"
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+  Graticule,
+} from "react-simple-maps"
+
+const wrapperStyles = {
+  width: "100%",
+  maxWidth: 980,
+  margin: "0 auto",
+}
+
+class GraticuleMap extends Component {
+  render() {
+    return (
+      <div style={wrapperStyles}>
+        <ComposableMap
+          projectionConfig={{
+            scale: 205,
+            rotation: [0,0,0],
+          }}
+          width={980}
+          height={551}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          >
+          <ZoomableGroup center={[0,0]}>
+            <Geographies geographyUrl="/static/world-50m.json">
+              {(geographies, projection) => geographies.map((geography, i) => (
+                <Geography
+                  key={i}
+                  geography={geography}
+                  projection={projection}
+                  style={{
+                    default: {
+                      fill: "#ECEFF1",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#607D8B",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#FF5722",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                  }}
+                />
+              ))}
+            </Geographies>
+            <Graticule />
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    )
+  }
+}
+
+export default GraticuleMap
