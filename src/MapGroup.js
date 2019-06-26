a1de6f849e962be02b31b24cd92ab983758f451b
Александр Поляков <ahushh@gmail.com>
2019-06-26 09:58:49 +0300

    Remove mouse events from markers

--- src/MapGroup.js
+++ src/MapGroup.js
@@ -1,46 +1,33 @@
-
 import React, { Component } from "react"
 
-import { Svg } from 'expo'
-const { G } = Svg;
+import { Svg } from "expo"
+const { G } = Svg
 
 class MapGroup extends Component {
   render() {
-
-    const {
-      children,
-      projection,
-      style,
-      zoom,
-      width,
-      height,
-      groupName,
-      itemName,
-    } = this.props
+    const { children, projection, style, zoom, width, height, groupName, itemName } = this.props
     return (
-      <G className={`rsm-${groupName}`} style={style}>
-        {
-          !children ?
-            null :
-            children.length === undefined ?
-              React.cloneElement(children, {
-                projection,
-                zoom,
-                width,
-                height,
-              }) :
-              children.map((child, i) =>
-                !child ?
-                  null :
-                  React.cloneElement(child, {
+      <G style={style}>
+        {!children
+          ? null
+          : children.length === undefined
+          ? React.cloneElement(children, {
+              projection,
+              zoom,
+              width,
+              height,
+            })
+          : children.map((child, i) =>
+              !child
+                ? null
+                : React.cloneElement(child, {
                     key: child.key || `${itemName}-${i}`,
                     projection,
                     zoom,
                     width,
                     height,
                   })
-              )
-        }
+            )}
       </G>
     )
   }
