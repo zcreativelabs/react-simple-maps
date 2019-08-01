dcd69966e8a2e8d59375b580f7dd6335184bb35f
zimrick <richard@zcreativelabs.com>
2019-08-01 14:58:43 +0200

    Added rollup

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+
+import babel from "rollup-plugin-babel"
+import babelrc from "babelrc-rollup"
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
+import { terser } from "rollup-plugin-terser"
+
+import pkg from "./package.json"
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+]
+
+export default [{
+  input: "src/index.js",
+  external,
+  output: {
+    name: "reactSimpleMaps",      
+    file: pkg.browser,
+    format: "umd",
+    globals: {
+      react: "React",
+      "react-dom": "ReactDOM",
+      "d3-geo": "d3-geo",
+      "topojson-client": "topojson",
+      "prop-types": "PropTypes",
+    },
+  },
+  plugins: [
+    babel(babelrc()),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+}, {
+  input: "src/index.js",
+  external,
+  output: [{
+      file: pkg.main,
+      format: "cjs"
+    },
+    {
+      file: pkg.module,
+      format: "es"
+    },
+  ],
+  plugins: [
+    babel(babelrc()),
+  ],
+}]
