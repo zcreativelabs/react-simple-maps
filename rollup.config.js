0ffbd0a2ed4f077a6fa473ee805542346402cf4e
zimrick <richard@zcreativelabs.com>
2022-07-06 20:45:35 +0200

    Updated dev packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,54 @@
+import { babel } from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 
-import babel from "rollup-plugin-babel"
-import babelrc from "babelrc-rollup"
-import resolve from "rollup-plugin-node-resolve"
-import commonjs from "rollup-plugin-commonjs"
-import { terser } from "rollup-plugin-terser"
-
-import pkg from "./package.json"
+import pkg from "./package.json";
 
 const external = [
   ...Object.keys(pkg.dependencies || {}),
   ...Object.keys(pkg.peerDependencies || {}),
-]
+];
 
-export default [{
-  input: "src/index.js",
-  external,
-  output: {
-    name: "reactSimpleMaps",      
-    file: pkg.browser,
-    format: "umd",
-    extend: true,
-    globals: {
-      react: "React",
-      "react-dom": "ReactDOM",
-      "d3-geo": "d3",
-      "d3-zoom": "d3",
-      "d3-selection": "d3",
-      "topojson-client": "topojson",
-      "prop-types": "PropTypes",
+export default [
+  {
+    input: "src/index.js",
+    external,
+    output: {
+      name: "reactSimpleMaps",
+      file: pkg.browser,
+      format: "umd",
+      extend: true,
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+        "d3-geo": "d3",
+        "d3-zoom": "d3",
+        "d3-selection": "d3",
+        "topojson-client": "topojson",
+        "prop-types": "PropTypes",
+      },
     },
+    plugins: [
+      babel({ babelHelpers: "bundled" }),
+      resolve(),
+      commonjs(),
+      terser(),
+    ],
   },
-  plugins: [
-    babel(babelrc()),
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-}, {
-  input: "src/index.js",
-  external,
-  output: [{
-      file: pkg.main,
-      format: "cjs"
-    },
-    {
-      file: pkg.module,
-      format: "es"
-    },
-  ],
-  plugins: [
-    babel(babelrc()),
-  ],
-}]
+  {
+    input: "src/index.js",
+    external,
+    output: [
+      {
+        file: pkg.main,
+        format: "module",
+      },
+      {
+        file: pkg.module,
+        format: "es",
+      },
+    ],
+    plugins: [babel({ babelHelpers: "bundled" })],
+  },
+];
